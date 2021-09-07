3aa4a03ddfebfb1c078f742c2c56cf053b165e2d
Pranav from Mark III <pranav.bhawan@gmail.com>
2021-09-08 03:30:03 +0530

    Errorsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- Lab1: Magic Sqaures/Final Game/header_play_game.h
+++ Lab1: Magic Sqaures/Final Game/header_play_game.h
@@ -12,6 +12,10 @@ void fill_center(map<int,vector<int>>* game_status);
 void get_current_boards(map<int,vector<int>> game_status);
 void move_pc_ai(map<int,vector<int>>* game_status);
 vector<int> get_user_move();
+void print_map(map<int,vector<int>> game_status);
+void print_map_user(map<int,vector<int>> game_status);
+void print_map_machine(map<int,vector<int>> game_status);
+vector<int> print_total_score(map<int,vector<int>> game_status);
 
 void running_game(map<int,vector<int>>* game_status)
 {
@@ -28,19 +32,19 @@ void running_game(map<int,vector<int>>* game_status)
 
     // A variable move count to keep track of moves made in the game
     int move_count = 0;
-    
+    vector<int> score;
     // User first or mchine first
     int type = 1;//default case for debugging and testing
 
     // TODO
-    cout<<"Welcome to the Tic Tak Toe Blah blah blah";
+    cout<<"\nWelcome to the Tic Tak Toe Blah blah blah";
     
     string name;
-    cout<<"Enter you name: ";
+    cout<<"\nEnter you name: ";
     cin>>name;
 
     char play_first;
-    cout<<"Hello "<<name<<" this is an AI. Nice to meet you! Would you like to play first (y->yes or n->no): ";
+    cout<<"\nHello "<<name<<" this is an AI. Nice to meet you! Would you like to play first (y->yes or n->no): ";
     cin>> play_first;
 
     if(play_first=='n')
@@ -51,7 +55,7 @@ void running_game(map<int,vector<int>>* game_status)
     {
         type = 2;// User first
     }
-
+    
     while(move_count<27) // Total at max 27 moves possible
     {
         // Being a general AI, check for a vacant center for first two chances        
@@ -64,45 +68,132 @@ void running_game(map<int,vector<int>>* game_status)
         //     fill_center(game_status);
         // }    
         
+        
         // Considering only machine first
-        if(move_count%2==0) //ML Casse
+        if(type==1)
         {
-            // First move, fill center
-            if(move_count==0) 
+            if(move_count%2==0) //ML Casse
             {
-                fill_center(game_status);
-            }    
+                // First move, fill center
+                if(move_count==0) 
+                {
+                    fill_center(game_status);
+                }    
+                else
+                {
+                    move_pc_ai(game_status);
+                }
+
+                printf("\nYou made a nice move, here's the board: \n");
+                
+                // // Can comment these
+                get_current_boards(*game_status);
+                // // print_map(*game_status);
+                print_map_machine(*game_status);
+                print_map_user(*game_status);
+            }
             else
             {
-                move_pc_ai(game_status);
-            }
-        }
-        else
-        {
-            // User input part is done for now
-            vector<int> user_move = get_user_move();
-            user_move.push_back(2); //For user
-            bool filled_or_not = fill(user_move, game_status);
-            while(1)
-            {
-                if(filled_or_not)
-                    break;
-
-                
-                get_current_boards(*game_status);
-                cout<<"\n-------ERROR BLOCK IS ALREADY FILLED------\nPlease enter correct vacant blocks: ";
-                user_move = get_user_move();
+                // User input part is done for now
+                vector<int> user_move = get_user_move();
                 user_move.push_back(2); //For user
-                filled_or_not = fill(user_move, game_status);
-            }             
-        } 
-        
-        printf("You made a nice move, here's the board: \n");
-        
-        get_current_boards(*game_status);
+                // cout<<"\n\n-<<<----"<<user_move[3]<<"-----\n\n";
+                bool filled_or_not = fill(user_move, game_status);
+                while(1)
+                {
+                    if(filled_or_not)
+                        break;
 
+                    // uncomment
+                    get_current_boards(*game_status);
+                    cout<<"\n-------ERROR BLOCK IS ALREADY FILLED------\nPlease enter correct vacant blocks: ";
+                    user_move = get_user_move();
+                    user_move.push_back(2); //For user
+                    filled_or_not = fill(user_move, game_status);
+                }             
+            }  
+   
+        }
+        
+        if(type==2)
+        {
+            if(move_count%2==1) //ML Casse
+            {
+                // First move, fill center
+                // TODO Error here in case 2, human first..
+                // Reason, due to no value in machine's list, we are unable to iterate over the elements
+                // In other cases, one element was there at centre
+                // So error when human occupies the centre first
+                if(move_count==1) 
+                {
+                    fill_center(game_status);
+                }    
+                else
+                {
+                    move_pc_ai(game_status);
+                }
+
+                printf("\nYou made a nice move, here's the board: \n");
+                
+                // // Can comment these
+                get_current_boards(*game_status);
+                // // print_map(*game_status);
+                print_map_machine(*game_status);
+                print_map_user(*game_status);
+            }
+            else
+            {
+                // User input part is done for now
+                vector<int> user_move = get_user_move();
+                user_move.push_back(2); //For user
+                // cout<<"\n\n-<<<----"<<user_move[3]<<"-----\n\n";
+                bool filled_or_not = fill(user_move, game_status);
+                while(1)
+                {
+                    if(filled_or_not)
+                        break;
+
+                    // uncomment
+                    get_current_boards(*game_status);
+                    cout<<"\n-------ERROR BLOCK IS ALREADY FILLED------\nPlease enter correct vacant blocks: ";
+                    user_move = get_user_move();
+                    user_move.push_back(2); //For user
+                    filled_or_not = fill(user_move, game_status);
+                }             
+            }  
+
+        }
+
+        if(move_count>6)
+        {
+            score = print_total_score(*game_status);
+
+            if(score[0]>=10)
+            {
+                cout<<"\n---------------------\nAI Won! Nice game.";
+                cout<<"\nFINAL SCOREBOARD IS: ";
+                print_total_score(*game_status);
+                return;
+            }
+            if(score[1]>=10)
+            {
+                cout<<"\n---------------------\nCongrats! You Won "<<name<<"! Nice game.";
+                cout<<"\nFINAL SCOREBOARD IS: ";
+                print_total_score(*game_status);
+                return;
+            }
+
+        }
         move_count++;
     }    
+
+    cout<<"\n----------------------";
+    cout<<"\n----------------------";
+    if(score[0]<10&&score[1]<10)
+    cout<<"\n------------------------\nDRAW!\n";
+    cout<<"\nFINAL SCOREBOARD IS: ";
+    print_total_score(*game_status);
+
 }
 
 
@@ -110,13 +201,18 @@ void fill_center(map<int,vector<int>>* game_status)
 {
     // making a default move to center if it is empty
     vector<int> move = {1,1,1,1}; // Filling center with 1 if it is vacant
-    fill(move, game_status);
+    bool test = fill(move, game_status);
+    if(!test)
+    {
+        move = {2,2,2,1};
+        fill(move,game_status);
+    }
 }
 
 bool fill(vector<int> move, map<int,vector<int>>* game_status)
 {
     bool filled_or_not = false;
-    for(auto it = game_status->cbegin(); it!= game_status->cend(); it++)
+    for(auto it = game_status->cbegin(); it != game_status->cend(); it++)
     {
         int x = move[0];
         int y = move[1];
@@ -139,6 +235,7 @@ vector<int> get_user_move()
     vector<int> response;
 
     int x,y,z;
+    cout<<"\nEnter (x,y,z) for your moves: ";
     cin>>x>>y>>z;
     
     response.push_back(x);
@@ -192,38 +289,45 @@ void move_pc_ai(map<int,vector<int>>* game_status)
             int a = machine_values[i];
             int b = machine_values[j];
             int diff = 42-a-b;
-            int vacancy = (*game_status)[diff][3];// To check whether it is vacant or not
 
-            if(vacancy==-1 && diff>=1 && diff<=27)
-            {
-                vector<int> point1 = (*game_status)[a];
-                vector<int> point2 = (*game_status)[b];
-                vector<int> point3 = (*game_status)[diff];
-                
-                int x1 = point1[0];
-                int x2 = point2[0];
-                int x3 = point3[0];
+            // VERY VERY IMPORTANT, GO RO CHECKING FOR DIFF IN HASH MAP 
+            // ONLY IF THE VALUE OF DIFF IS FROM 1 TO 27 ELSE THERE WOULD BE SEGMENTATION ERROR
+            if(diff>=1 && diff<=27)
+            {               
+                int vacancy = (*game_status)[diff][3];// To check whether it is vacant or not
+                // printf("-----%d------\n",vacancy);
 
-                int y1 = point1[1];
-                int y2 = point2[1];
-                int y3 = point3[1];
-
-                int z1 = point1[2];
-                int z2 = point2[2];
-                int z3 = point3[2];
-                
-                bool linear_or_not = check_linearity(x1,y1,z1,x2,y2,z2,x3,y3,z3);
-                
-                if(linear_or_not)
+                if(vacancy==-1)
                 {
-                    vector<int> move = {x3,y3,z3,1}; // Picking the third point
-                    filled = fill(move, game_status); // Filling the third point if it is collinear
+                    vector<int> point1 = (*game_status)[a];
+                    vector<int> point2 = (*game_status)[b];
+                    vector<int> point3 = (*game_status)[diff];
                     
-                    // If returned and successful fill above, then return out of this function else fo further
-                    if(filled)
-                        return;
-                }
-            } 
+                    int x1 = point1[0];
+                    int x2 = point2[0];
+                    int x3 = point3[0];
+
+                    int y1 = point1[1];
+                    int y2 = point2[1];
+                    int y3 = point3[1];
+
+                    int z1 = point1[2];
+                    int z2 = point2[2];
+                    int z3 = point3[2];
+                    
+                    bool linear_or_not = check_linearity(x1,y1,z1,x2,y2,z2,x3,y3,z3);
+                    
+                    if(linear_or_not)
+                    {
+                        vector<int> move = {x3,y3,z3,1}; // Picking the third point
+                        filled = fill(move, game_status); // Filling the third point if it is collinear
+                        
+                        // If returned and successful fill above, then return out of this function else fo further
+                        if(filled)
+                            return;
+                    }
+                } 
+            }
         }
     }
 
@@ -235,53 +339,205 @@ void move_pc_ai(map<int,vector<int>>* game_status)
             int a = user_values[i];
             int b = user_values[j];
             int diff = 42-a-b;
-            int vacancy = (*game_status)[diff][3];// To check whether it is vacant or not
-
-            if(vacancy==-1 && diff>=1 && diff<=27)
+            if(diff>=1 && diff<=27)
             {
-                vector<int> point1 = (*game_status)[a];
-                vector<int> point2 = (*game_status)[b];
-                vector<int> point3 = (*game_status)[diff];
-                
-                int x1 = point1[0];
-                int x2 = point2[0];
-                int x3 = point3[0];
+                int vacancy = (*game_status)[diff][3];// To check whether it is vacant or not
+                // printf("-----%d------\n",vacancy);
 
-                int y1 = point1[1];
-                int y2 = point2[1];
-                int y3 = point3[1];
-
-                int z1 = point1[2];
-                int z2 = point2[2];
-                int z3 = point3[2];
-                
-                bool linear_or_not = check_linearity(x1,y1,z1,x2,y2,z2,x3,y3,z3);
-                
-                // Exact same logic just on user now, if user in winnig position, then fill and block it
-                if(linear_or_not)
+                if(vacancy==-1)
                 {
-                    vector<int> move = {x3,y3,z3,1}; // Picking the third point
-                    filled = fill(move, game_status); // Filling the third point if it is collinear
+                    vector<int> point1 = (*game_status)[a];
+                    vector<int> point2 = (*game_status)[b];
+                    vector<int> point3 = (*game_status)[diff];
                     
-                    // If returned and successful fill above, then return out of this function else fo further
-                    if(filled)
-                        return;
+                    int x1 = point1[0];
+                    int x2 = point2[0];
+                    int x3 = point3[0];
+
+                    int y1 = point1[1];
+                    int y2 = point2[1];
+                    int y3 = point3[1];
+
+                    int z1 = point1[2];
+                    int z2 = point2[2];
+                    int z3 = point3[2];
+                    
+                    bool linear_or_not = check_linearity(x1,y1,z1,x2,y2,z2,x3,y3,z3);
+                    
+                    // Exact same logic just on user now, if user in winnig position, then fill and block it
+                    if(linear_or_not)
+                    {
+                        vector<int> move = {x3,y3,z3,1}; // Picking the third point
+                        filled = fill(move, game_status); // Filling the third point if it is collinear
+                        
+                        // If returned and successful fill above, then return out of this function else fo further
+                        if(filled)
+                            return;
+                    }
                 }
             } 
         }
     }
 
     // TODO Needs Optimisation
-    if(!filled)
+    int i=0;
+    while(!filled)
     {
         // Pick x,y,z of first vacant point
-        vector<int> x = (*game_status)[vacant_values[0]];
+        vector<int> x = (*game_status)[vacant_values[i]];
         vector<int> move = {x[0],x[1],x[2],1};
         filled = fill(move, game_status);
+        // printf("\n\n-------RAndom fill-------\n\n");
     }
     // printf("\n\n-------------%d------\n\n",filled);
     // for(int i=0;i<6;i++)
     // {
     //     printf("%d ",machine_values[i]);
     // }
+}
+
+void print_map(map<int,vector<int>> game_status)
+{
+    cout<<"Value:  x, y, z, \tChance\n";
+    for(auto it = game_status.cbegin(); it!=game_status.cend(); ++it)
+    {
+        cout<<it->first<<": \t"<<it->second[0]<<", "<<it->second[1]<<", "<<it->second[2]<<", \t"<<it->second[3]<<endl;
+    }
+}
+
+void print_map_user(map<int,vector<int>> game_status)
+{
+    cout<<"\nUSER MOVES\n";
+    cout<<"Value:  x, y, z, \tChance\n";
+    for(auto it = game_status.cbegin(); it!=game_status.cend(); ++it)
+    {
+        if(it->second[3]==2)
+        cout<<it->first<<": \t"<<it->second[0]<<", "<<it->second[1]<<", "<<it->second[2]<<", \t"<<it->second[3]<<endl;
+    }
+}
+
+void print_map_machine(map<int,vector<int>> game_status)
+{
+    cout<<"\nMACHINE MOVES\n";
+    cout<<"Value:  x, y, z, \tChance\n";
+    for(auto it = game_status.cbegin(); it!=game_status.cend(); ++it)
+    {
+        if(it->second[3]==1)
+        cout<<it->first<<": \t"<<it->second[0]<<", "<<it->second[1]<<", "<<it->second[2]<<", \t"<<it->second[3]<<endl;
+    }
+}
+
+vector<int> print_total_score(map<int,vector<int>> game_status)
+{
+    int user_score = 0;
+    int machine_score = 0;
+    vector<int> machine_values;
+    vector<int> user_values;
+    vector<int> vacant_values;
+
+    for(auto it = game_status.cbegin(); it!= game_status.cend(); it++)
+    {
+        int switcher = it->second[3];
+        switch (switcher)
+        {
+        case 1:
+            machine_values.push_back(it->first);
+            break;
+
+        case 2:
+            user_values.push_back(it->first);
+            break;
+
+        default:
+            vacant_values.push_back(it->first);
+            break;
+        }
+    }
+
+    for(int i=0;i<user_values.size()-2;i++)
+    {
+        for(int j=i+1;j<user_values.size()-1;j++)
+        {
+            for(int k = j+1; k<user_values.size(); k++)
+            {
+                int a = user_values[i];
+                int b = user_values[j];
+                int c = user_values[k];
+                if(a+b+c==42)
+                {
+                    vector<int> point1 = (game_status)[a];
+                    vector<int> point2 = (game_status)[b];
+                    vector<int> point3 = (game_status)[c];
+
+                    int x1 = point1[0];
+                    int x2 = point2[0];
+                    int x3 = point3[0];
+
+                    int y1 = point1[1];
+                    int y2 = point2[1];
+                    int y3 = point3[1];
+
+                    int z1 = point1[2];
+                    int z2 = point2[2];
+                    int z3 = point3[2];
+                    
+                    bool linear_or_not = check_linearity(x1,y1,z1,x2,y2,z2,x3,y3,z3);
+                    
+                    if(linear_or_not)
+                    {
+                        user_score++;
+                    }
+                }
+            }
+        }
+    }
+
+for(int i=0;i<machine_values.size()-2;i++)
+    {
+        for(int j=i+1;j<machine_values.size()-1;j++)
+        {
+            for(int k = j+1; k<machine_values.size(); k++)
+            {
+                int a = machine_values[i];
+                int b = machine_values[j];
+                int c = machine_values[k];
+                if(a+b+c==42)
+                {
+                    vector<int> point1 = (game_status)[a];
+                    vector<int> point2 = (game_status)[b];
+                    vector<int> point3 = (game_status)[c];
+
+                    int x1 = point1[0];
+                    int x2 = point2[0];
+                    int x3 = point3[0];
+
+                    int y1 = point1[1];
+                    int y2 = point2[1];
+                    int y3 = point3[1];
+
+                    int z1 = point1[2];
+                    int z2 = point2[2];
+                    int z3 = point3[2];
+                    
+                    bool linear_or_not = check_linearity(x1,y1,z1,x2,y2,z2,x3,y3,z3);
+                    
+                    if(linear_or_not)
+                    {
+                        machine_score++;
+                    }
+                }
+            }
+        }
+    }
+
+    vector<int> score = {machine_score, user_score};
+
+    // same for machine
+
+    cout<<"\n-----------------------------------------------\nSCOREBOARD\nUser(X): ";
+    cout<<user_score<<"\nMachine(O): "<<machine_score<<"\n";
+
+    return score;
+
+
 }
--- Lab1: Magic Sqaures/Final Game/main.cpp
+++ Lab1: Magic Sqaures/Final Game/main.cpp
@@ -19,15 +19,16 @@ using namespace std;
 
 int main(void)
 {
-    square magic_1(3);
-    magic_1.add_basic_values(2);
-    // magic_1.print_square();
+    // TEST CASES: IGNORE
+    // square magic_1(3);
+    // magic_1.add_basic_values(2);
+    // // magic_1.print_square();
 
-    square magic_2(3);
-    magic_2.add_basic_values(3);
-    // magic_2.print_square();
+    // square magic_2(3);
+    // magic_2.add_basic_values(3);
+    // // magic_2.print_square();
 
-    magic_1.next_sqaure(&magic_2);
+    // magic_1.next_sqaure(&magic_2);
     // magic_1.next->print_square();
 
     magicCube myCube(3);
@@ -36,7 +37,7 @@ int main(void)
     cin>>x>>y>>z;
     myCube.make_magic_cube(x,y,z);
     myCube.print_cube();
-    myCube.start_game();
+    // myCube.start_game();
     // cout<<check_linearity(1,11,121,0,4,0,1,5,0);
     map<int,vector<int>>* game_status = myCube.get_game_status();
 
