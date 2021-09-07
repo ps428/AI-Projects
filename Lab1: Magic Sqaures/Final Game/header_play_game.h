2f18a1a0c9c494555573bfee3a89b17d94d20006
Pranav from Mark III <pranav.bhawan@gmail.com>
2021-09-07 23:11:10 +0530

    Added Basic AI Approach, able to get powwin for machine

--- Lab1: Magic Sqaures/Final Game/header_play_game.h
+++ Lab1: Magic Sqaures/Final Game/header_play_game.h
@@ -10,6 +10,7 @@ bool fill(vector<int> move, map<int,vector<int>>* game_status);
 void running_game(map<int,vector<int>>* game_status);
 void fill_center(map<int,vector<int>>* game_status);
 void get_current_boards(map<int,vector<int>> game_status);
+void move_pc_ai(map<int,vector<int>>* game_status);
 vector<int> get_user_move();
 
 void running_game(map<int,vector<int>>* game_status)
@@ -27,7 +28,7 @@ void running_game(map<int,vector<int>>* game_status)
 
     // A variable move count to keep track of moves made in the game
     int move_count = 0;
-
+    
     // User first or mchine first
     int type = 1;//default case for debugging and testing
 
@@ -68,10 +69,13 @@ void running_game(map<int,vector<int>>* game_status)
         {
             // First move, fill center
             if(move_count==0) 
+            {
                 fill_center(game_status);
-
-
-
+            }    
+            else
+            {
+                move_pc_ai(game_status);
+            }
         }
         else
         {
@@ -150,4 +154,134 @@ void get_current_boards(map<int,vector<int>> game_status)
     magicCube a = magicCube(3);
     a.make_cube_using_map(game_status);
     a.print_game_cube();
+}
+
+void move_pc_ai(map<int,vector<int>>* game_status)
+{
+    // These three linked lists will hold the keys from hashmap 
+    // based on the values filled in their postions on the boards
+    vector<int> machine_values;
+    vector<int> user_values;
+    vector<int> vacant_values;
+    bool filled = false; // A bool to check if it is filled or not
+
+    for(auto it = game_status->cbegin(); it!= game_status->cend(); it++)
+    {
+        int switcher = it->second[3];
+        switch (switcher)
+        {
+        case 1:
+            machine_values.push_back(it->first);
+            break;
+
+        case 2:
+            user_values.push_back(it->first);
+            break;
+
+        default:
+            vacant_values.push_back(it->first);
+            break;
+        }
+    }
+    
+    // kind of poss win for machine function and fill if in a winning position
+    for(int i=0;i<machine_values.size()-1;i++)
+    {
+        for(int j=i+1;j<machine_values.size();j++)
+        {
+            int a = machine_values[i];
+            int b = machine_values[j];
+            int diff = 42-a-b;
+            int vacancy = (*game_status)[diff][3];// To check whether it is vacant or not
+
+            if(vacancy==-1 && diff>=1 && diff<=27)
+            {
+                vector<int> point1 = (*game_status)[a];
+                vector<int> point2 = (*game_status)[b];
+                vector<int> point3 = (*game_status)[diff];
+                
+                int x1 = point1[0];
+                int x2 = point2[0];
+                int x3 = point3[0];
+
+                int y1 = point1[1];
+                int y2 = point2[1];
+                int y3 = point3[1];
+
+                int z1 = point1[2];
+                int z2 = point2[2];
+                int z3 = point3[2];
+                
+                bool linear_or_not = check_linearity(x1,y1,z1,x2,y2,z2,x3,y3,z3);
+                
+                if(linear_or_not)
+                {
+                    vector<int> move = {x3,y3,z3,1}; // Picking the third point
+                    filled = fill(move, game_status); // Filling the third point if it is collinear
+                    
+                    // If returned and successful fill above, then return out of this function else fo further
+                    if(filled)
+                        return;
+                }
+            } 
+        }
+    }
+
+    // Poss win kind of something for user, if user in wiing position, then block it
+    for(int i=0;i<user_values.size()-1;i++)
+    {
+        for(int j=i+1;j<user_values.size();j++)
+        {
+            int a = user_values[i];
+            int b = user_values[j];
+            int diff = 42-a-b;
+            int vacancy = (*game_status)[diff][3];// To check whether it is vacant or not
+
+            if(vacancy==-1 && diff>=1 && diff<=27)
+            {
+                vector<int> point1 = (*game_status)[a];
+                vector<int> point2 = (*game_status)[b];
+                vector<int> point3 = (*game_status)[diff];
+                
+                int x1 = point1[0];
+                int x2 = point2[0];
+                int x3 = point3[0];
+
+                int y1 = point1[1];
+                int y2 = point2[1];
+                int y3 = point3[1];
+
+                int z1 = point1[2];
+                int z2 = point2[2];
+                int z3 = point3[2];
+                
+                bool linear_or_not = check_linearity(x1,y1,z1,x2,y2,z2,x3,y3,z3);
+                
+                // Exact same logic just on user now, if user in winnig position, then fill and block it
+                if(linear_or_not)
+                {
+                    vector<int> move = {x3,y3,z3,1}; // Picking the third point
+                    filled = fill(move, game_status); // Filling the third point if it is collinear
+                    
+                    // If returned and successful fill above, then return out of this function else fo further
+                    if(filled)
+                        return;
+                }
+            } 
+        }
+    }
+
+    // TODO Needs Optimisation
+    if(!filled)
+    {
+        // Pick x,y,z of first vacant point
+        vector<int> x = (*game_status)[vacant_values[0]];
+        vector<int> move = {x[0],x[1],x[2],1};
+        filled = fill(move, game_status);
+    }
+    // printf("\n\n-------------%d------\n\n",filled);
+    // for(int i=0;i<6;i++)
+    // {
+    //     printf("%d ",machine_values[i]);
+    // }
 }
