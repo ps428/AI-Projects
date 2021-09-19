0fd296c09daa246ee14ee7cc3d18b1c5b40d87fd
Pranav from Mark III <pranav.bhawan@gmail.com>
2021-09-19 13:53:28 +0530

    Able to add a basic screen

---
+++ Assignment_2/Checkers_Game/board.py
@@ -0,0 +1,34 @@
+import pygame
+import piece
+import values
+
+class game_Board:
+    def __init__(self):
+        self.board = []
+        self.player_count_black = 12
+        self.player_count_white = 12
+        self.kings_count_black = 0
+        self.kings_count_white = 0
+
+        
+    def fill_block(window, row, column):
+        # Fill a rectangle at row, column with height and width equal to BLOCK_SIZE
+        pygame.draw.rect(window, values.LIGHT, (row*values.BLOCK_SIZE, column*values.BLOCK_SIZE, values.BLOCK_SIZE, values.BLOCK_SIZE))
+
+    def draw_blocks(self, window):
+        window.fill(values.DARK)
+        for row in range(values.ROWS):
+            for column in range(values.COLUMNS):
+                # For even columns, fill even rows with light
+                if column%2==0:
+                    if row%2!=0:
+                        # print("--------")
+                        pygame.draw.rect(window, values.LIGHT, (row*values.BLOCK_SIZE, column*values.BLOCK_SIZE, values.BLOCK_SIZE, values.BLOCK_SIZE))
+                else:
+                    if row%2==0:
+                        pygame.draw.rect(window, values.LIGHT, (row*values.BLOCK_SIZE, column*values.BLOCK_SIZE, values.BLOCK_SIZE, values.BLOCK_SIZE))
+                        
+                    
+    
+
+
---
+++ Assignment_2/Checkers_Game/main.py
@@ -0,0 +1,52 @@
+import pygame
+import values
+import game
+import board
+
+# Setting the dimensions of the window
+window = pygame.display.set_mode((values.WIDTH, values.HEIGHT))
+# Adding a name to the window
+pygame.display.set_caption("Checker's Game with Min Max")
+
+# Main driver function
+def main():
+    # Playing as true initially
+    playing = True
+    
+    # TEsting basic display
+    new_board = board.game_Board()
+    new_board.draw_blocks(window)
+    pygame.display.update()
+
+    while playing:
+
+        # Look at the events being triggered by the user in the game
+        for event in pygame.event.get():
+            # If a event to end the game is triggered, then quit the game
+            if event.type == pygame.QUIT:
+                playing = False
+        
+            # If the event is mouse button cliked, then do something
+            # NOTE: MOUSEBUTTONDOWN => button is clicked
+            #       MOUSEBUTTONUP   => button is released
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                position = pygame.get_pos()
+                row, column = get_mouse_position(position)
+
+        
+        pygame.display.update()
+        
+    pygame.quit()
+
+
+
+
+def get_mouse_position(position):
+    a,b = position
+    
+    row = a//values.BLOCK_SIZE
+    column = b//values.BLOCK_SIZE
+
+    return row, column
+
+main()
