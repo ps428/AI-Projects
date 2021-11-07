e2b2280bd2bc5577d05be4986760668f6d2441a9
Pranav from Mark III <pranav.bhawan@gmail.com>
2021-11-07 18:12:03 +0530

    TO save output to a file, just simply do, python3 filename.py >> out.txt

--- Assignment4/Goal Stack/file_reader.py
+++ Assignment4/Goal Stack/file_reader.py
@@ -10,8 +10,8 @@ for line in lines:
     data = (line.split(":"))
     a.append(data[1].strip())
 
-initial_state = main.generate_state(a[0],a[1],a[2],a[3],a[4])
-goal_state = main.generate_state(a[5],a[6],a[7],a[8],a[9])
+initial_state = main.generate_state(a[1],a[2],a[3],a[4],a[5])
+goal_state = main.generate_state(a[8],a[9],a[10],a[11],a[12])
 
 goal_stack = planner.GSP(initial_state=initial_state, goal_state=goal_state)
 plan_queue = goal_stack.get_operations()
