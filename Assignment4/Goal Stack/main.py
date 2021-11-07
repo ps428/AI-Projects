afda190aaf8cd504924986c269a1e9ed72525725
Pranav from Mark III <pranav.bhawan@gmail.com>
2021-11-07 16:43:35 +0530

    started with usser input

---
+++ Assignment4/Goal Stack/main.py
@@ -0,0 +1,9 @@
+import planner
+
+print("Enter Initial State: ")
+on_predicates = input("Enter ON predicates seperated by commas (E.g. [A,B],[D,C]): ")
+on_table_predicates = input("Enter ONTABLE predicates seperated by commas(E.g. B,C): ")
+clear_predicates = input("Enter CLEAR predicates seperated by commas(E.g. A,D): ")
+arm_empty = input("Is arm empty: y or n")
+
+
